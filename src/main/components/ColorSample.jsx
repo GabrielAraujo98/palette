4bf103cee5cac71e8f5b1948d09a21d0a669f19f
GabrielAraujo98 <56730579+GabrielAraujo98@users.noreply.github.com>
2019-12-19 15:22:09 -0300

    hex tag

--- src/main/components/ColorSample.jsx
+++ src/main/components/ColorSample.jsx
@@ -2,13 +2,20 @@ import React, { Component } from 'react'
 
 export default class ColorSample extends Component {
 
-    
+
 
     render() {
         return (
-            <section className={`hero`} style={{backgroundColor: this.props.style}}>
+            <section className={`hero`} style={{ backgroundColor: this.props.style }}>
                 <div className='hero-body'>
-                    <h1 className={`has-text-black tag is-white-ter is-medium`}>{this.props.style}</h1>
+                    <span className='tags has-addons '>
+                        <div className='tag is-medium is-rounded'>
+                            Hex code:
+                        </div>
+                        <div className='tag is-medium title has-text-black is-rounded'>
+                            {this.props.style}
+                        </div>
+                    </span>
                 </div>
             </section>
         )
