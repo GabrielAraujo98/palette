7e5848e1e9693774810871d3af819388db53806d
GabrielAraujo98 <56730579+GabrielAraujo98@users.noreply.github.com>
2019-12-23 14:48:06 -0300

    Palette Tile

--- src/main/selector/PaletteSelctor.jsx
+++ src/main/selector/PaletteSelctor.jsx
@@ -6,8 +6,10 @@ export default class PaletteSelctor extends Component {
             <div>
                 <h1 className='title is-1 has-text-centered has-text-danger'>Welcome to Palette!</h1>
                 <p className='subtitle is-3 has-text-centered has-text-black'>Select the your Palette size:</p>
-                <div class="tile has-text-centered">
-                    
+                <div class="tile is-parent is-vertical">
+                    <article class="tile is-child notification is-white sombreado">
+                        <p class="">Vertical...</p>
+                    </article>
                 </div>
             </div>
         )
