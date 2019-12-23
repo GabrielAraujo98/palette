3ca3fa4ad9d1aa5d157393490fa7870a4079a491
GabrielAraujo98 <56730579+GabrielAraujo98@users.noreply.github.com>
2019-12-23 14:19:47 -0300

    PaletteSelector

--- src/main/selector/PaletteSelctor.jsx
+++ src/main/selector/PaletteSelctor.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 
 export default class PaletteSelctor extends Component {
     render() {
@@ -7,17 +6,9 @@ export default class PaletteSelctor extends Component {
             <div>
                 <h1 className='title is-1 has-text-centered has-text-danger'>Welcome to Palette!</h1>
                 <p className='subtitle is-3 has-text-centered has-text-black'>Select the your Palette size:</p>
-                <div class="control has-text-centered">
-                    <label class="radio">
-                        <input type="radio" name="answer" />
-                        3
-                    </label>
-                    <label class="radio">
-                        <input type="radio" name="answer" />
-                        5
-                    </label>
+                <div class="tile has-text-centered">
+                    
                 </div>
-                <Link to='/colorgenerator' className='button is-rounded is-danger'>Choose!</Link>
             </div>
         )
     }
