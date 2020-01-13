4d202e9474435e709e4064f4028136cd758e8c70
GabrielAraujo98 <56730579+GabrielAraujo98@users.noreply.github.com>
2020-01-13 14:33:33 -0300

    Updates

---
+++ precache-manifest.bc64129e7c81b3d07df00ecb4b065f6a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c8c0da03d82b897fad7c37658f9ee1",
+    "url": "/palette/index.html"
+  },
+  {
+    "revision": "7e2c045fb8922ef4e9c3",
+    "url": "/palette/static/css/2.cb15ee85.chunk.css"
+  },
+  {
+    "revision": "c44dd59adba1c79ca510",
+    "url": "/palette/static/css/main.4b997fc8.chunk.css"
+  },
+  {
+    "revision": "7e2c045fb8922ef4e9c3",
+    "url": "/palette/static/js/2.30e4df2c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/palette/static/js/2.30e4df2c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c44dd59adba1c79ca510",
+    "url": "/palette/static/js/main.e14e9b73.chunk.js"
+  },
+  {
+    "revision": "4b1b984384d226d89f98",
+    "url": "/palette/static/js/runtime-main.86b5a6fc.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/palette/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/palette/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/palette/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/palette/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/palette/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "3c04ea03d6fe375f34f7bee396dabe3d",
+    "url": "/palette/static/media/palette-logo.3c04ea03.png"
+  }
+]);
