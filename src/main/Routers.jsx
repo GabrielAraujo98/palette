3106ac52c974b2b316b68432ebf94ca2d3c6ad69
GabrielAraujo98 <gabrielsa1114@gmail.com>
2025-02-11 18:40:59 -0300

    Fix: definindo o componente de paletas sugeridas como a tela inicial da aplicação

--- src/main/Routers.jsx
+++ src/main/Routers.jsx
@@ -5,15 +5,11 @@ import Cadastro from './../components/Cadastro'
 import ColorGenerator from './generator/ColorGenerator'
 import PaletteSelctor from './selector/PaletteSelctor'
 import PaletteSugestions from '../components/PaletteSugestions/PaletteSugestions';
-import PalettesGrid from '../components/Assets/PalettesGrid/PalettesGrid';
-import apiService from '../services/apiServices';
 export default class Routers extends Component {
 
     constructor(props) {
         super(props)
     }
-
-
 
     render() {
             return (
@@ -26,12 +22,9 @@ export default class Routers extends Component {
                                 functionThree={this.props.selectFunctionThree}
                                 functionFive={this.props.selectFunctionFive}
                             />}/>
-                        <Route path="/colorgenerator" element={<ColorGenerator function={this.props.function} colors={this.props.colors} />}/>
-                        <Route exact path="/*" element={<PaletteSelctor
-                                functionThree={this.props.selectFunctionThree}
-                                functionFive={this.props.selectFunctionFive}
-                            />}/>
-                        <Route path="/home" element={<PaletteSugestions function={this.props.function}/>}/>
+                        <Route exact path="/colorgenerator" element={<ColorGenerator function={this.props.function} colors={this.props.colors} />}/>
+                        <Route exact path="/*" element={<PaletteSugestions function={this.props.function}/>}/>
+                        <Route exact path="/home" element={<PaletteSugestions function={this.props.function}/>}/>
                     </Routes>
                     </div>
                 </Router>
