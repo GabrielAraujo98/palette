91460a4899840869de6d6b6dbbb84052fd95f148
GabrielAraujo98 <gabrielsa1114@gmail.com>
2025-02-25 14:29:04 -0300

    Fix: Ajustando o grid das paletas sugeridas para não ter espaçoes grandes entre uma paleta e outra.

--- src/components/Assets/PalettesGrid/PalettesGrid.jsx
+++ src/components/Assets/PalettesGrid/PalettesGrid.jsx
@@ -6,20 +6,27 @@ import CardColorPreview from '../CardColorPreview/CardColorPreview';
 const PalettesGrid = ({allPalettes}) => {
     let samples = []
     let [palettes, setPalettes] = useState([])
+    let columns = [0, 1, 2, 3, 4, 5]
+    let cols = columns
 
-     for (let i = 0; i < allPalettes.length; i++) {
-        
+    for(let j = 0; j < columns.length; j++){
+
+      for (let i = 0; i < allPalettes.length; i++) {
         let colorsString = allPalettes[i][2].replace('{', '').replace('}', '')
         let colors = colorsString.split(',')
-        
-        samples.push(
-          <CardColorPreview key={i} palette={colors} schemeLength={allPalettes[i][1]}/>
-        )
+
+        if((i) % (columns.length) == j){
+          samples.push(<CardColorPreview key={i} palette={colors} schemeLength={allPalettes[i][1]}/>)
+        }
       }
+
+      cols[j] = <div className='grid-col'>{samples}</div>;
+      samples = []
+    }
 
       useEffect(() => {
         setTimeout(() => {
-          setPalettes(samples)
+          setPalettes(cols)
         }, 1)
       })
 
